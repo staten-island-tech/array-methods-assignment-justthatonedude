a57abc08fa5808fb1201f2db01ffe0e1ab623d35
justthatonedude <jothamgao@gmail.com>
2023-10-10 10:20:31 -0400

    1

---
+++ app.js
@@ -0,0 +1,16 @@
+const sillyism = [
+    {
+        numArray : [2314,23432,5436],
+        dumb : true,
+    },
+    {
+        numArray : [232,12034,1203],
+        dumb: false,
+    },
+]
+sillyism.forEach((obj) =>{
+    
+}),
+sillyism.forEach((obj) => {
+    obj.numArray.forEach((num) => console.log(num))
+})
