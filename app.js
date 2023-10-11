5f77683b5a117734fd385c4638706267c629cd86
justthatonedude <jothamgao@gmail.com>
2023-10-11 10:13:39 -0400

    3

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const sillyism = [
     {
-        numArray : [2314,23432,5436],
+        Name : "yes",
+        numArray : ["2314","23432","5436"],
         dumb : true,
     },
     {
-        numArray : [232,12034,1203],
+        Name: "no",
+        numArray : ["232","12034","1203"],
         dumb: false,
     },
 ]
-sillyism.forEach((obj) =>{
-    
-}),
-sillyism.forEach((obj) => {
-    obj.numArray.forEach((num) => console.log(num))
-})
+function printName(sillyism){
+    sillyism.forEach((obj) => console.log(obj.Name));
+};
+function printnumArray(sillyism){
+    sillyism.forEach((obj) => obj.numArray.forEach((num) => console.log(num)));
+};
+
+function printnewArray(sillyism){
+    const newArray = sillyism.filter((obj)=>obj.dumb === true);
+    newArray.forEach((obj) => obj.numArray.forEach((num) => console.log(num)));
+};
+
+printnewArray(sillyism)
+printnumArray(sillyism)
+printName(sillyism)
